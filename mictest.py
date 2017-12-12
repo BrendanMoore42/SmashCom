c9688ec465a70a3dff6b777a796cc675aa897c52
Brendan Moore <brendanm42@gmail.com>
2017-12-12 18:59:31 -0500

    Fixed Terminal Loop Error

--- mictest.py
+++ mictest.py
@@ -1,89 +1,65 @@
-import speech_recognition as sr
 from directkeys import *
 import time
 from moves import *
+import speech_recognition as sr
 
 
-# # get audio from the microphone
-# r = sr.Recognizer()
-# with sr.Microphone() as source:
-#     print("Speak:")
-#     audio = r.listen(source)
-#
-# try:
-#     if r.recognize_google(audio) == "up smash":
-#         print ("Great upsmash!!")
-#     else:
-#         print("You said " + r.recognize_google(audio))
-# except sr.UnknownValueError:
-#     print("Could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results; {0}".format(e))
+options = ["jump", "left", "right", "crouch", "down", "up",
+         "a", "b", "grab", "shield", "jab", "smash"]
 
-for i in list(range(2))[::-1]:#Counts down from x then executes code
-    print (i+1)
-    time.sleep(1)
-
-
-#
-# while True:
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print ("Say the move: ")
-#         audio = r.listen(source)
-#         new_audio = [audio]
-#         for word in new_audio:
-#             if word == moves:
-#
-
-
-# punch = ["punch", "jab", "a"]
-moves = ["jump", "left", "right", "crouch", "down", "up",
-         "a", "b", "grab", "shield", "jab", "smash", "hold"]
-
-def mic_on():
-    with sr.Microphone() as source:
-        print ("Chill calibrating")
-        r.adjust_for_ambient_noise(source, duration=5)
-        print("Say a move:")
-        audio = r.listen(source)
+mods = ["hold", "smash", "tilt", "double", "once", "twice", "triple", "quadruple", "wait"]
 
 r = sr.Recognizer()
 
-# r.pause_threshold = 1.0
-# r.phrase_threshold = 1.0
-# r.non_speaking_duration = 0.35
-
 moves = []
-
 while True:
-    # with sr.Microphone() as source:
-    #     # print("Holy shit chill calibrating")
-    #     # r.adjust_for_ambient_noise(source, duration=5)
-    #     print("Say a move:")
-    #     audio = r.listen(source)
     with sr.Microphone() as source:
-        print("Show me your moves:")
-        audio = r.listen(source)
-        moves.append(r.recognize_google(audio))
-        print(moves)
-        new_moves = [words for segments in moves for words in segments.split()]
-        print(new_moves)
-        try:
-            if "jump" in new_moves:
-                jump()
-            elif "left" in new_moves:
-                left()
-            else:
-                print("You said " + r.recognize_google(audio))
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
         audio = []
         moves = []
         new_moves = []
-
-
-
+        try:
+            print("Show me your moves:")
+            audio = r.listen(source)
+            moves.append(r.recognize_google(audio))
+            print(moves)
+            new_moves = [words for segments in moves for words in segments.split()]
+            print(new_moves)
+            for action in new_moves:
+                try:
+                    if action == "jump":
+                        jump()
+                    elif action == "laser":
+                        laser()
+                    elif action == "double-laser":
+                        shdl()
+                    elif action == "up":
+                        up()
+                    elif action == "left":
+                        left()
+                    elif action == "right":
+                        right()
+                    elif action == "down" or action == "crouch":
+                        crouch()
+                    elif action == "punch" or action == "Jab":
+                        jab()
+                    elif action == "double" and action == "Jab":
+                        djab()
+                    elif action == "Shield":
+                        shield()
+                    elif action == "grab":
+                        grab()
+                    elif action == "right smash":
+                        Rsmash()
+                    elif action == "left smash":
+                        Lsmash()
+                    elif action == "shine":
+                        shine()
+                    else:
+                        print("You didn't say any moves...")
+                except sr.UnknownValueError:
+                    print("Could not understand audio")
+                except sr.RequestError as e:
+                    print("Could not request results; {0}".format(e))
+        except:
+            pass
 
